53189b3274f556bb3e66dc5811401de5e08178d1
Julia <juliet.yor@gmail.com>
2022-09-15 19:18:14 +0300

    Условие If-Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine()!;
+
+if(username.ToLower() == "катя")
+{
+Console.WriteLine("Ура, это мой пирожочек!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
